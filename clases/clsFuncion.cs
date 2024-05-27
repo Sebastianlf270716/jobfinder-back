ba0b67c1c776a1b118edca35bf116f79bbfdc118
Sebastianlf270716 <sebastianlopez325226@correo.itm.edu.co>
2024-05-26 21:22:53 -0500

    Crear oferta, falta añadir llenar la tabla gestion

---
+++ clases/clsFuncion.cs
@@ -0,0 +1,26 @@
+﻿using jobfinder_back.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobfinder_back.clases
+{
+    public class clsFuncion
+    {
+        private jobfinderEntities jobfinder = new jobfinderEntities();
+
+        public void Insertar(Funcion funcion)
+        {
+            try
+            {
+                jobfinder.Funcions.Add(funcion);
+                jobfinder.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
